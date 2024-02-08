45fd02ad0ff7368736e000239b8535a57a3b7bd0
Enes YAPICI <enesyapici28@gmail.com>
2024-02-08 11:01:19 +0300

    project completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./navbar.css";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 export default function Navbar() {
   return (
     <div className="navbar">
       <div className="container">
-        <Link to="/">Home</Link>
-        <Link to="/posts">Posts</Link>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/posts">Posts</NavLink>
       </div>
     </div>
   );
